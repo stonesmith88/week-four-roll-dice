5b6fc82b8634116063efd1fa88663d2dba47e932
Kevin Bost <kevbost@gmail.com>
2014-05-21 14:49:30 -0400

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,3 +11,11 @@ function square(x) {
 function roll(dice) {
 	
 }
+
+// Should return 2
+// Does the click button generate the 2
+// Does the click button itself animate when clicked &:hover'd
+// Clicking div.a should &.active div.bA div.c
+// Click should animate div.bA
+// div.c should display the correct value returned by random
+// 
